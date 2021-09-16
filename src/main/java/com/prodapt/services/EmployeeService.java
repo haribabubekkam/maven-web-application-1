d5d13c91859342559ce0254181513e6b71410d0a
ratnakishorech <78347867+ratnakishorech@users.noreply.github.com>
2021-09-16 11:32:02 +0530

    Update EmployeeService.java

--- src/main/java/com/prodapt/services/EmployeeService.java
+++ src/main/java/com/prodapt/services/EmployeeService.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+
 @RequestMapping("/employee")
 public class EmployeeService {
 
@@ -28,6 +28,6 @@ public class EmployeeService {
 		js.put("Position", "Sr.Software Engineer");
 		js.put("IT Services", "Cloud Platform");
 
-		return js.parseInt();
+		return js.toString();
 }
 }
