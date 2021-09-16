9a252743ba31d7b842280e3f27afa5101f075cd2
ratnakishorech <78347867+ratnakishorech@users.noreply.github.com>
2021-09-16 11:27:32 +0530

    Update EmployeeService.java

--- src/main/java/com/prodapt/services/EmployeeService.java
+++ src/main/java/com/prodapt/services/EmployeeService.java
@@ -3,7 +3,7 @@ package com.prodapt.services;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
+import java.io.NullPointerException;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -20,7 +20,7 @@ public class EmployeeService {
 	@RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
 	@ResponseBody
 	String uploadImage(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession)
-			throws IOException {
+			throws NullPointerException  {
 
 		JSONObject js = new JSONObject();
 		js.put("Name", "Madhusudhanachari");
